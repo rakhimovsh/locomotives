f333d092afecfaaa2ed39f80fe578ab51496f49e
rakhimovsh <shukurulloh.com@gmail.com>
2022-12-21 12:08:21 +0400

    changed

--- src/pages/Admin/AddLaboratory.jsx
+++ src/pages/Admin/AddLaboratory.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { Form, Input } from 'antd';
+import styled from 'styled-components';
+
+const { Item } = Form;
+
+const Wrapper = styled.div`
+  margin-top: 25px;
+`;
 
 const AddLaboratory = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const subjectId = searchParams.get('subjectId');
   const laboratoryId = searchParams.get('laboratoryId');
   return (
-    <div>
-
-    </div>
+    <Wrapper>
+      <Form
+        layout="vertical"
+      >
+        <Item name="title" label="Sarlavha" rules={[{ required: true, message: 'Maydon to\'ldirilishi shart' }]}>
+          <Input placeholder="Sarlavha..." />
+        </Item>
+      </Form>
+    </Wrapper>
   );
 };
 
